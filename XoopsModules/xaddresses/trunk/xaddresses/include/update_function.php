db4e4b217fc8576a263a1dd41127b42b3e259544
luciorota <luciorota@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-07-24 21:20:35 +0000

    delete changed files/directories

--- XoopsModules/xaddresses/trunk/xaddresses/include/update_function.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('DS') or define('DS', DIRECTORY_SEPARATOR);
-
-function xoops_module_update_xaddresses(&$xoopsModule, $oldVersion = null) {
-    return true;
-}
-?>
--- XoopsModules/xaddresses/trunk/xaddresses/language/english/formmultiplexoopsimage.php
+++
@@ -1,6 +0,0 @@
-<?php
-//class/formmultiplexoopsimage.php
-define("_FORMMULTIPLEXOOPSIMAGE", "Image[%s]");
-define("_FORMMULTIPLEXOOPSIMAGE_NEW", "Add New Image");
-define("_FORMMULTIPLEXOOPSIMAGE_DELETE", "Remove Image");
-?>
