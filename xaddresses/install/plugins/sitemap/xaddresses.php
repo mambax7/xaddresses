e530d055da001dec0ef0a846193c4e0c206da266
Lucio Rota <lucio.rota@gmail.com>
2012-07-24 21:20:35 +0000

    delete changed files/directories

--- xaddresses/install/plugins/sitemap/xaddresses.php
+++
@@ -1,7 +0,0 @@
-<?php
-function b_sitemap_xaddresses(){
-	$xoopsDB =& Database::getInstance();
-    $block = sitemap_get_categoires_map($xoopsDB->prefix("xaddresses_locationcategory"), "cat_id", "cat_pid", "cat_title", "categoryview.php?cat_id=", "cat_title");
-	return $block;
-}
-?>
